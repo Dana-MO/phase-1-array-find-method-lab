ac2f3a12c3779ff61283dee985d3aad71ac4a606
danaomar <danaomaark@gmail.com>
2023-10-03 13:45:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(arr) {
+  const winningGame = arr.find(isWin);
+  function isWin(game) {
+    return (game.result === "W");
+  }
+  if(winningGame === undefined) {
+    return undefined;
+  } else {
+    return winningGame.year;
+  }
+}
